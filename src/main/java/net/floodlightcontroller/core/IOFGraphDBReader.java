334b43ae5e4dc2b5048b28811619d5a13f496904
gopidirajesh <rajesh@cs.unc.edu>
2013-10-25 11:22:56 -0400

    missing interface

---
+++ src/main/java/net/floodlightcontroller/core/IOFGraphDBReader.java
@@ -0,0 +1,12 @@
+/*
+ * Author: Rajesh Gopidi
+ * A Graduate student at UNC Chapel Hill
+ *
+ */
+
+package net.floodlightcontroller.core;
+
+public interface IOFGraphDBReader {
+  
+    public void startParsing();
+}
